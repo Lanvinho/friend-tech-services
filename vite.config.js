ed1b55821b6b579738cf5a30ae70e049b9fa3968
Lanvinho <lanvindeffeu@gmail.com>
2025-03-02 03:19:44 +0100

    Initial commit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './', // Racine du projet
-  build: {
-    outDir: 'dist', // Dossier de sortie pour les fichiers construits
-  },
-});
